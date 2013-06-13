1eb1a823e56656c3dfb979a196f08f9b601b5703
case <jeremy.collake@gmail.com>
2013-06-13 11:55:26 -0400

    simple, unpolished script to create a reversion firmware from the vendor firmware and sysupgrade aftermarket firmware

---
+++ create-reversion-firmware.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# creates a reversion firmware via transplant of contents
+#
+SRC="$1"
+DEST="$2"
+
+if [ "$UID" != "0" ]; then
+        SUDO="sudo"
+else
+        SUDO=""
+fi
+
+if [ "$1" == "-h" ] || [ ! -e "$SRC" ] || [ ! -e "$DEST" ]; then
+	echo "Usage: $0 [VENDORIMG] [SYSUPGRADE]"
+	echo "Example: $0 tl-wdr4300-vendor.bin gargoyle-wdr4300-sysupgrade.bin"
+	exit 1
+fi
+sudo rm -rf vendor
+sudo rm -rf fmk
+./extract-firmware.sh "$SRC"
+mv fmk vendor
+./extract-firmware.sh "$DEST"
+./transplant-firmware.sh vendor fmk
+./build-firmware.sh
+echo "Done! Reversion firmware saved as fmk/new-firmware.bin"
+
+
