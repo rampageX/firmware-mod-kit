b389d9604fa0f56e6ee9bef86a3b99300eb8d74e
case <jeremy.collake@gmail.com>
2013-06-13 16:29:39 -0400

    minor

--- transplant-firmware.sh
+++ transplant-firmware.sh
@@ -4,16 +4,8 @@
 # does not migrate firmware format information, allowing an image to 
 # be rebuilt into a different format. Particularly useful for reversion.
 #
-# jc
-#
 SRC="$1"
 DEST="$2"
-
-if [ "$UID" != "0" ]; then
-        SUDO="sudo"
-else
-        SUDO=""
-fi
 
 if [ "$1" == "-h" ] || [ ! -d "$SRC/image_parts" ] || [ ! -d "$DEST/image_parts" ]; then
 	echo "Usage: $0 [SRC] [DEST]"
@@ -21,10 +13,16 @@ if [ "$1" == "-h" ] || [ ! -d "$SRC/image_parts" ] || [ ! -d "$DEST/image_parts"
 	exit 1
 fi
 
+if [ "$UID" != "0" ]; then
+        SUDO="sudo"
+else
+        SUDO=""
+fi
+
 $SUDO rm -rf "$DEST/rootfs.bak"
 $SUDO rm -rf "$DEST/image_parts.bak"
-$SUDO mv -f "$DEST/rootfs" "$DEST/rootfs.bak"
-$SUDO mv -f "$DEST/image_parts" "$DEST/image_parts.bak"
+mv -f "$DEST/rootfs" "$DEST/rootfs.bak"
+mv -f "$DEST/image_parts" "$DEST/image_parts.bak"
 $SUDO cp -rf "$SRC/rootfs" "$DEST/"
 $SUDO cp -rf "$SRC/image_parts" "$DEST/"
 
